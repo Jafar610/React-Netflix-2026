b82dd199a36adfb14e8eca65e505ce49c8b562be
Jafar610 <jafarmohamed955@gmail.com>
2026-01-31 23:07:01 +0300

    utils added

---
+++ src/utils/request.jsx
@@ -0,0 +1,13 @@
+const API_KEY = "87e8125d9e0a02912d4f407ca67a79d6";
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networkds=213`,
+    fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies : `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies : `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies : `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies : `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries : `/discover/movie?api_key=${API_KEY}&with_genres=59`,
+    fetchTvShow: `tv/popular?api_key=${API_KEY}&language=en-US&page=1`
+}
+export default requests;
