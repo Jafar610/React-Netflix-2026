477f419af6cd4dc020bbac459b01ef5e4ca6a000
Jafar610 <jafarmohamed955@gmail.com>
2026-02-07 23:35:02 +0300

    file updated

--- src/components/Rows/Row/Row.jsx
+++ src/components/Rows/Row/Row.jsx
@@ -1,64 +1,68 @@
-import { useEffect, useState } from "react";
-import axios from "../../../utils/axios.jsx";
-import "./Row.css";
-import YouTube from "react-youtube";
-import movieTrailer from "movie-trailer";
-const Row = ({title, fetchUrl, isLargeRow}) => {
-  const [movie, setMovie] = useState([]);
-  const [trailerURL, setTrailerURL] = useState([]);
-  const base_URL = "https://image.tmdb.org/t/p/original";
+import React, { useEffect, useState } from 'react'
+import "./row.css";
+import axios from "../../../utils/axios";
+import movieTrailer from 'movie-trailer';
+import YouTube from 'react-youtube';
 
-  useEffect(() => {
-    (async ()=>{
-      try{
-        const request = await axios.get(`${fetchUrl}`);
-        setMovie(request.data.results);
-      } catch(error){
-        console.log("Error",error);
-      }
-    })()
-  }, []);
+const Row = ({ title, fetchUrl, isLargeRow }) => {
+    const [movies, setMovie] = useState([]);
+    const [trailerUrl, setTrailerUrl] = useState("");
 
-  const handleClick = (movie)=>{
-    if(trailerURL){
-      setTrailerURL('')
-    }else{
-      movieTrailer(movie?.title || movie?.name || movie?.original_name)
-      .then((url)=>{
-        const urlParams = new URLSearchParams(new URL(url).search);
-        setTrailerURL(urlParams.get('v'));
-      })
-    }
-  }
+    const base_url = "https://image.tmdb.org/t/p/original";
 
-  const opts = {
-    height: '390',
-    width: '100%',
-    playerVars:{
-      autoplay:1,
-    },
-  }
 
-  return (
-    <div className="row">
-      <h1>{title}</h1>
-      <div className="row__posters">
-        {
-          movie?.map((movie, index)=>(
-            <img 
-            onClick={()=>handleClick(movie)}
-            key={index}
-            src={`${base_URL}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
-            alt={movie.name}
-            className={`row_poster ${isLargeRow && "row_posterLarge"}`} />
-          ))
+    useEffect(() => {
+        (async () => {
+            try {
+                console.log(fetchUrl)
+                const request = await axios.get(`${fetchUrl}`);
+                console.log(request)
+                setMovie(request.data.results);
+            } catch (error) {
+                console.log("error", error);
+            }
+        })()
+    }, [fetchUrl]);
+
+    const handleClick = (movie) => {
+        if (trailerUrl) {
+            setTrailerUrl('')
+        } else {
+            movieTrailer(movie?.title || movie?.name || movie?.original_name)
+                .then((url) => {
+                    console.log(url)
+                    const urlParams = new URLSearchParams(new URL(url).search)
+                    console.log(urlParams)
+                    console.log(urlParams.get('v'))
+                    setTrailerUrl(urlParams.get('v'));
+                })
         }
-      </div>
-      <div style={{padding: '40px'}}>
-        {trailerURL && <YouTube videoId = {trailerURL} opts = {opts}/>}
-      </div>
-    </div>
-  );
-};
+    }
 
-export default Row;
+    const opts = {
+        height: '390',
+        width: "100%",
+        playerVars: {
+            autoplay: 1,
+        },
+    }
+
+    return (
+        <div className="row">
+            <h1>{title}</h1>
+            <div className="row__posters">
+                {movies?.map((movie, index) => (
+                    <img
+                        onClick={() => handleClick(movie)}
+                        key={index} src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name} className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                    />
+                ))}
+            </div>
+            <div style={{ padding: '40px' }}>
+                {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+            </div>
+        </div>
+    )
+}
+
+export default Row
