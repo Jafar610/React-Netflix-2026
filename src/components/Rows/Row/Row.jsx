90b738f4c276fae9e279a4d8b3464d00d0a5192a
Jafar610 <jafarmohamed955@gmail.com>
2026-02-09 23:11:44 +0300

    file updated

--- src/components/Rows/Row/Row.jsx
+++ src/components/Rows/Row/Row.jsx
@@ -7,7 +7,6 @@ import YouTube from "react-youtube";
 const Row = ({ title, fetchUrl, isLargeRow }) => {
   const [movies, setMovie] = useState([]);
   const [trailerUrl, setTrailerUrl] = useState("");
-
   const base_url = "https://image.tmdb.org/t/p/original";
 
   useEffect(() => {
